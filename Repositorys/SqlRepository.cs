813cb96d8fb02c6d1a59bc03014e80751e6ec2d2
Grymnir <magnus.carl.ekman@gmail.com>
2022-01-05 23:45:50 +0100

    Add project files.

---
+++ Repositorys/SqlRepository.cs
@@ -0,0 +1,47 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace WiredBrainCoffee.StorageApp.Repositorys
+{
+    
+    public class SqlRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public SqlRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public event EventHandler<T>? ItemAdded;
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.OrderBy(item => item.Id).ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T item)
+        {
+            _dbSet.Add(item);
+            ItemAdded?.Invoke(this, item);
+        }
+        public void Remove(T item)
+        {
+            _dbSet.Remove(item);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
